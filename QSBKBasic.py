93eda69d57ee382f39fbca23de311df9f8acd46d
OliverTse2012 <olivertse2012@gmail.com>
2016-05-15 11:22:12 +0800

    Update QSBKBasic.py

--- QSBKBasic.py
+++ QSBKBasic.py
@@ -4,6 +4,7 @@
 import urllib.request
 import re
 
+__author__ = 'XC'
 page = 1
 weburl = "http://www.qiushibaike.com/hot/page/" + str(page)
 webheader = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:23.0) Gecko/20100101 Firefox/23.0'}
